e7abf33e8c448cd5c25b21e5e304341402cb4cd2
csorexr <69866302+csorexr@users.noreply.github.com>
2020-09-28 11:06:24 -0700

    snake facing left or right based on player

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
+function checkFacing () {
+    sprite_list = sprites.allOfKind(SpriteKind.Enemy)
+    for (let value of sprite_list) {
+        if (plane.x > value.x && sprites.readDataBoolean(value, "facingLeft")) {
+            value.image.flipX()
+            sprites.setDataBoolean(value, "facingLeft", false)
+        } else if (plane.x < value.x && !(sprites.readDataBoolean(value, "facingLeft"))) {
+            value.image.flipX()
+            sprites.setDataBoolean(value, "facingLeft", true)
+        }
+    }
+}
+let sprite_list: Sprite[] = []
 let mySprite2: Sprite = null
+let plane: Sprite = null
 scene.setBackgroundColor(15)
 tiles.setTilemap(tiles.createTilemap(hex`1400080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000002000000000000000000040500000000000000040500000000000000000203060500000000000203070000000000000000040606070000000000040607000000000101010101010101010101010101010101010101`, img`
     . . . . . . . . . . . . . . . . . . . . 
@@ -10,7 +24,7 @@ tiles.setTilemap(tiles.createTilemap(hex`140008000000000000000000000000000000000
     . . . . 2 2 2 2 . . . . . 2 2 2 . . . . 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     `, [myTiles.transparency16,sprites.dungeon.hazardHole,myTiles.tile1,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundEast], TileScale.Sixteen))
-let mySprite = sprites.create(img`
+plane = sprites.create(img`
     .......ff...............
     ....ffff2ff.............
     ..ffeeeef2ff............
@@ -36,8 +50,8 @@ let mySprite = sprites.create(img`
     ........................
     ........................
     `, SpriteKind.Player)
-mySprite.setFlag(SpriteFlag.StayInScreen, true)
-controller.moveSprite(mySprite)
+plane.setFlag(SpriteFlag.StayInScreen, true)
+controller.moveSprite(plane)
 for (let value of tiles.getTilesByType(myTiles.tile1)) {
     mySprite2 = sprites.create(img`
         . . . . c c c c c c . . . . . . 
@@ -58,11 +72,13 @@ for (let value of tiles.getTilesByType(myTiles.tile1)) {
         . . c c c c c c c c c f . . . . 
         `, SpriteKind.Enemy)
     tiles.placeOnTile(mySprite2, value)
+    sprites.setDataBoolean(mySprite2, "facingLeft", true)
     tiles.setTileAt(value, myTiles.transparency16)
 }
 game.onUpdateInterval(100, function () {
     if (scene.cameraProperty(CameraProperty.X) < 240) {
         scene.centerCameraAt(scene.cameraProperty(CameraProperty.X) + 1, scene.cameraProperty(CameraProperty.Y))
-        mySprite.x += 1
+        plane.x += 1
     }
+    checkFacing()
 })
